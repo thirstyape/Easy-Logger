b19ce4c4739d8bf35b6e8e210b5ebfabb71d508d
Nathanael <nf@nathanaelfrey.ca>
2023-08-24 17:34:05 -0400

    Create MAUI app for log parsing

---
+++ Easy-Logger-Parser/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Easy_Logger_Parser;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Easy-Logger-Parser/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Easy_Logger_Parser;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
